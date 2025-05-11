8d45b8d3e2d0187ffd19fd1adc44e19b76a0436d
nampdabbank <nampd@abbank.vn>
2025-05-11 20:57:04 +0700

    update domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/nampd-portfolio",
+  base: "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
